f15121476b1af0a9a5f0d35f28e8b469fd504497
kuyawa <haxapp@gmail.com>
2019-11-24 23:00:14 -0400

    First commit

--- bot.py
+++ bot.py
@@ -1,24 +1,69 @@
-from flask import Flask, request
+import os
 import requests
+from flask import Flask, request, current_app, abort
+from functools import wraps
+from twilio.request_validator import RequestValidator
 from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
+# Environment variables
+
+TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
+GATEWAY = os.environ.get('GATEWAY') or 'localhost:8000/gateway'
+APP_DEBUG = (TWILIO_AUTH_TOKEN is None)
+
+print('Token',TWILIO_AUTH_TOKEN)
+print('debug?',APP_DEBUG)
+
+
+# Twilio validation
+
+def validate_twilio_request(f):
+    """Validates that incoming requests genuinely originated from Twilio"""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        # Create an instance of the RequestValidator class
+        validator = RequestValidator(TWILIO_AUTH_TOKEN)
+
+        # Validate the request using its URL, POST data and X-TWILIO-SIGNATURE header
+        request_valid = validator.validate(
+            request.url,
+            request.form,
+            request.headers.get('X-TWILIO-SIGNATURE', ''))
+
+        # Continue processing the request if it's valid, return a 403 error if it's not
+        if request_valid or current_app.debug:
+            return f(*args, **kwargs)
+        else:
+            return abort(403)
+    return decorated_function
+
+
+# Routing
 
 @app.route('/', methods=['GET'])
 def index():
     return 'Hello'
 
+
 @app.route('/test', methods=['GET'])
 def test():
     return 'Test'
-    
+
+
 @app.route('/bot', methods=['POST'])
+@validate_twilio_request
 def bot():
     txt = request.values.get('Body', '').lower()
     res = MessagingResponse()
-    # msg = res.message() #ERROR!!!
     ok  = False
+    
+    if not gateway: 
+        text = 'Paysapp is down. Try again later'
+        res.message(text)
+        return str(res)
+
 
     if 'hello' in txt:
         # Register phone
@@ -27,28 +72,30 @@ def bot():
         ok = True
 
     if 'balance' in txt:
-        # return balance in default currency
-        #r = requests.get('https://api.example.com/balance/PHONEID/USD')
-        #if r.status_code == 200:
-        #    data = r.json()
-        #    quote = '{"balance": 125.00}'
-        #else:
-        #    quote = 'Balance unavailable, try again in a minute'
+        # Default currency balance
         text = 'Your balance is 125.00 USD'
         res.message(text)
         ok = True
 
     if 'pay' in txt:
-        # split parts, verify info and send money
-        # msg.media('https://gateway.example.com/receipt')
         text = 'Payment sent'
         res.message(text)
         ok = True
 
+    if 'help' in action:
+        text = 'Valid commands: hello, pay, balance'
+        res.message(text)
+        ok = True
+
     if not ok:
-        res.message('Invalid action, RTFM!')
+        text = "Invalid action. Type 'help' for more info"
+        res.message(text)
 
     return str(res)
 
-#print("Server running...")
+
+
+if __name__ == '__main__':
+    app.run(debug=APP_DEBUG)
+
 #END
